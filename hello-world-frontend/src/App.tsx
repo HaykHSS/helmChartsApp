5228c90749e34314b914b833d2049e0c356b34f1
HaykHSS <hayk.sargsyan.2015@gmail.com>
2024-06-14 14:54:46 +0400

    bugfix

--- hello-world-frontend/src/App.tsx
+++ hello-world-frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   const handleGetText = () => {
     // fetch(`${import.meta.env.VITE_BACKEND_URL}`)
-    fetch('http://backend-chart.backend.svc.cluster.local')
+    fetch('http://backend-chart.backend.svc.cluster.local:3001')
       .then((res) => {
         setLoading(true);
         return res.json();
